cfa758987874b7a9c560c4364d0a2afefa5b590e
user <jahidanowar689@gmail.com>
2022-02-13 10:10:59 +0800

    baper-bank-added

---
+++ js/banking.js
@@ -0,0 +1,43 @@
+
+document.getElementById('deposit-button').addEventListener('click', function (){
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositText = depositInput.value;
+    const newDepositeAmount = parseFloat(newDepositText);
+    // console.log(depositAmount);
+
+    const depositTotal = document.getElementById('deposit-total');
+    const previousText = depositTotal.innerText;
+    const previousAmount = parseFloat(previousText);
+    const newAmount = previousAmount + newDepositeAmount;
+    
+    depositTotal.innerText = newAmount;
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositeAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+    depositInput.value = '';
+});
+
+document.getElementById('withdraw-button').addEventListener('click', function (){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawText);
+    
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(previousWithdrawText);
+    const withdrawTotalAmount = previousWithdrawAmount + newWithdrawAmount;
+    withdrawTotal.innerText = withdrawTotalAmount;
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - withdrawTotalAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    
+    withdrawInput.value = '';
+
+})
